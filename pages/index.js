6ed68ee2fb288823371477c7f0d8e2bfd0295d52
Ruchi Mutneja <mutnejaruchi@gmail.com>
2024-04-19 17:41:11 +0530

    adding action button & redirecting to a route using Next router

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 function Home() {
+  const router = useRouter()
+
+  const handleClick = () => {
+    console.log('placing order')
+    router.replace('/product') // or use router.push
+  }
+
   return ( 
   <div>
     <h1>Home Page</h1>
@@ -9,8 +17,11 @@ function Home() {
       Blog
     </Link>
     <Link href="/product">
-    Product
+    Products
     </Link>
+    <button onClick={handleClick}>
+      Place Order
+    </button>
 
   </div>
   )
